b55f967d39fa02784e274161dcf6e50285e5358d
Pragati Upadhayay <152602488+pragati-afk@users.noreply.github.com>
2026-09-04 14:06:20 +0530

    refactor: optimize server startup configuration and middleware initialization in server.js

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ app.use(cors(corsOptions));
 // Respond to all preflight requests
 app.options("*", cors(corsOptions));
 // ─────────────────────────────────────────────────────────────────────────────
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "public", "uploads")));
 
 // Routes
